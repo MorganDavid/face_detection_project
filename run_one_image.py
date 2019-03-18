fab0dd620c6b318350e6a34b24ed090bdbdb9810
MorganDavid <32820185+MorganDavid@users.noreply.github.com>
2019-03-18 12:36:31 +0000

    changed to using classes

--- run_one_image.py
+++ run_one_image.py
@@ -5,33 +5,62 @@ import cv2
 import numpy as np
 from keras.models import load_model
 import time
-
 class run_one_image():
 	DETC_TRHESH = 1.5e-11#The threashold for it being a face (higher = more sensitive) This is used when we need to detect more than one face in the image.harry: 1.5e11 1d: 1.5e4
-	NUM_FACES = 80 # This is used instead of DETEC_THREASH. Use the top_k_faces function instead. NUM_FACES=2 means take the top 2 most confident face boxes and draw them.
-	IM_WIDTH = 20 # this actually the dims the CNN was trained on. 
+	NUM_FACES = 30 # This is used instead of DETEC_THREASH. Use the top_k_faces function instead. NUM_FACES=2 means take the top 2 most confident face boxes and draw them.
+	IM_WIDTH = 20
 	IM_HEIGHT = 20
 	_model_dir = r'my_network_trained.h5'
-
-	def __init__(self):
-		print("made class")
 	'''
 	kernel_size is tupple: (height,width)
 	Returns generator. Loop over this function
 	'''
-	def make_sliding_window(self, image, step_size, kernel_size):
-		height, width, _ = image.shape
-		image = cv2.resize(image,(width, height))
-		x_up_to = width-kernel_size[0]-step_size
-		y_up_to = height-kernel_size[1]-step_size
+	def make_sliding_window(image, step_size, kernel_size):
+		
+		image = cv2.resize(image,(new_width, new_height))
+		x_up_to = new_width-kernel_size[0]-step_size
+		y_up_to = new_height-kernel_size[1]-step_size
 		for y in range(0,y_up_to,step_size):
 			for x in range(0,x_up_to,step_size):
 				yield (x,y,image[y:y+kernel_size[1],x:x+kernel_size[0]])
 				
-	
+	image = cv2.imread("1d_scaled.jpg")
+	height, width, depth = image.shape
+	print(image.shape)
+	resize_factor = 0.5
+	new_height, new_width = (int(height*resize_factor),int(width*resize_factor))
+	trumpim = cv2.resize(image,(new_width,new_height))
+	count = 0
+	images = []
+	positions = []
+	print("starting the sliding window loop. ")
+	start = time.time()
+	for x,y,im in make_sliding_window(trumpim,2, (IM_HEIGHT,IM_WIDTH)):#Don't change kernel size, resize original image instead. Step size can be changed. 
+		images.append(im)
+		positions.append((x,y))
+		count = count + 1
+
+	print("sliding window took ",time.time()-start, " seconds.")	
+	model =load_model(_model_dir)
+	images = np.asarray(images)
+
+	print("starting prediction")
+	start = time.time()
+	classes = model.predict(images, batch_size=10)
+	print("prediction took ",time.time()-start, " seconds.")
+
+	init_boxes = []; # will contain every box which has probability above the detection threashold. 
+	for i in range(0,len(classes)):
+		this_class = classes[i]
+		x,y = positions[i]
+		#print("confidence at x %d, y %d: "%(x,y),this_class)
+		if this_class<DETC_TRHESH:
+			#cv2.rectangle(trumpim, (x,y),(x+IM_WIDTH,y+IM_HEIGHT),(0,255,0),1) # draw all windows first. (no NMS)
+			init_boxes.append([x,y,x+IM_WIDTH,y+IM_HEIGHT]);
+
 	# Malisiewicz et al.
 	# This function was taken from https://www.pyimagesearch.com/2015/02/16/faster-non-maximum-suppression-python/
-	def non_max_suppression(self, boxes, overlapThresh):
+	def non_max_suppression(boxes, overlapThresh):
 		# if there are no boxes, return an empty list
 		if len(boxes) == 0:
 			return []
@@ -92,64 +121,25 @@ class run_one_image():
 	for box in supr_boxes:
 		cv2.rectangle(trumpim,(box[0],box[1]),(box[2],box[3]),(255,0,0),2)
 	'''
-	def draw_from_threashold(self, img, init_boxes):
-		supr_boxes = self.non_max_suppression(np.asarray(init_boxes),0.3);
+	def draw_from_threashold(init_boxes):
+		supr_boxes = non_max_suppression(np.asarray(init_boxes),0.3);
 		for box in supr_boxes:
-			cv2.rectangle(img,(box[0],box[1]),(box[2],box[3]),(255,0,0),2)
-	
+			cv2.rectangle(trumpim,(box[0],box[1]),(box[2],box[3]),(255,0,0),2)
+	draw_from_threashold(init_boxes)
 	#This method takes the top K confidence boxes and uses them. Also performs NMS.
-	def draw_top_k_rectangles(self, img, classes, positions,k):
+	def draw_top_k_rectangles(k):
 		indices = np.argpartition(classes.flatten(),k)[:k]
 		boxes_top_k_xy = [positions[i] for i in indices] # get x,y of every box
-		boxes_top_k = [[i[0],i[1],i[0]+self.IM_WIDTH,i[1]+self.IM_HEIGHT] for i in boxes_top_k_xy] # make them four coords. 
+		boxes_top_k = [[i[0],i[1],i[0]+IM_WIDTH,i[1]+IM_HEIGHT] for i in boxes_top_k_xy] # make them four coords. 
 		print(boxes_top_k)
-		boxes_nms = self.non_max_suppression(np.asarray(boxes_top_k),0.28)
+		boxes_nms = non_max_suppression(np.asarray(boxes_top_k),0.28)
 		for box in boxes_nms:
 			x,y=box[:2]
-			cv2.rectangle(img,(x,y),(x+self.IM_WIDTH,y+self.IM_HEIGHT),(0,255,0),1)
+			cv2.rectangle(trumpim,(x,y),(x+IM_WIDTH,y+IM_HEIGHT),(0,255,0),1)
 	#draw_top_k_rectangles(100)
 
-	'''
-	Runner method '''
-	def detect_faces(self, image):
-		height, width, depth = image.shape
-		print(image.shape)
-		resize_factor = 0.5
-		new_height, new_width = (int(height*resize_factor),int(width*resize_factor))
-		trumpim = cv2.resize(image,(new_width,new_height))
-		count = 0
-		images = []
-		positions = []
-		print("starting the sliding window loop. ")
-		start = time.time()
-		for x,y,im in self.make_sliding_window(trumpim,2, (self.IM_HEIGHT,self.IM_WIDTH)):#Don't change kernel size, resize original image instead. Step size can be changed. 
-			images.append(im)
-			positions.append((x,y))
-			count = count + 1
 
-		print("sliding window took ",time.time()-start, " seconds.")	
-		model =load_model(self._model_dir)
-		images = np.asarray(images)
-
-		print("starting prediction")
-		start = time.time()
-		classes = model.predict(images, batch_size=50)
-		print("prediction took ",time.time()-start, " seconds.")
-
-		init_boxes = []; # will contain every box which has probability above the detection threashold. 
-		for i in range(0,len(classes)):
-			this_class = classes[i]
-			x,y = positions[i]
-			#print("confidence at x %d, y %d: "%(x,y),this_class)
-			if this_class<self.DETC_TRHESH:
-				#cv2.rectangle(trumpim, (x,y),(x+IM_WIDTH,y+IM_HEIGHT),(0,255,0),1) # draw all windows first. (no NMS)
-				init_boxes.append([x,y,x+self.IM_WIDTH,y+self.IM_HEIGHT]);
-		self.draw_top_k_rectangles(trumpim,classes,positions,self.NUM_FACES)
-
-		cv2.imshow("t",trumpim)
-		cv2.waitKey()
-		cv2.destroyAllWindows()
-
+	cv2.imshow("t",trumpim)
+	cv2.waitKey()
+	cv2.destroyAllWindows()
 x = run_one_image()
-image = cv2.imread("scaled_image_harry.jpg")
-x.detect_faces(image)
